c6e53ff484225181519af4740494eb9c434e3c13
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-26 19:02:31 +0600

    Completed task 4

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,8 +6,8 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
   styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
-  intervalFired = new EventEmitter<number>();
-  interval: any
+  @Output() intervalFired = new EventEmitter<number>();
+  interval: any;
   lastNumber = 0;
 
   constructor() {}
